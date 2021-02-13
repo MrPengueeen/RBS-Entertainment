2a8eca4ed0cbd44705c4575c8c420dab245ddef6
MrPengueeen <aonmoy0.0@gmail.com>
2021-02-14 02:05:50 +0600

    disabled screen recording

--- lib/views/screens/home_screen/movie_widgets/play_movie.dart
+++ lib/views/screens/home_screen/movie_widgets/play_movie.dart
@@ -1,6 +1,7 @@
 import 'package:RBS/colors.dart';
 import 'package:RBS/views/screens/home_screen/movie_widgets/youtube_player_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 class PlayMovieScreen extends StatefulWidget {
   final url;
@@ -12,6 +13,16 @@ class PlayMovieScreen extends StatefulWidget {
 }
 
 class _PlayMovieScreenState extends State<PlayMovieScreen> {
+  Future<void> disableScreenRecord() async {
+    await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+  }
+
+  @override
+  void initState() {
+    disableScreenRecord();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
